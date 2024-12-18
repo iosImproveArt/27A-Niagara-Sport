8070143c95582117924ebab9da6cc81a0811c511
iosImproveArt <impiosart@gmail.com>
2024-12-18 15:29:33 +0200

    finall

---
+++ Niagara Sport/Niagara_SportApp.swift
@@ -0,0 +1,36 @@
+
+
+import SwiftUI
+
+@main
+struct Niagara_SportApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+var qefcqefc = "qfcqrfc"
+    var cwrfcwrf = 52525
+    func wrfcwrfc() -> Double {
+        return 13413.0134
+    }
+    func qecfqefce() {
+        print("qefcqef")
+        print("qefcqef")
+        print("qefcqef")
+    }
+        
+    var body: some Scene {
+        WindowGroup {
+            ContentViewNiagaraSport()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
+
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    static var orientationLock = UIInterfaceOrientationMask.all
+
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientationLock
+    }
+}
